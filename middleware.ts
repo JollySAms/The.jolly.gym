00541834cfe416b5430ea0162ae459b1606727e3
Casper Schukken <casper@schukken.com>
2026-07-30 14:23:17 +0200

    Remove Clerk proxy mode

--- middleware.ts
+++ middleware.ts
@@ -15,12 +15,11 @@ export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
     await auth.protect()
   }
-}, { frontendApiProxy: { enabled: true } })
+})
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
-    '/__clerk/(.*)',
   ],
 }
